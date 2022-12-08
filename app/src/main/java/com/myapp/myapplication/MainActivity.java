e576dc4eb58bf928801244d4515015c1f67b6088
nouser3 <heyiamhere05@gmail.com>
2022-12-08 21:43:36 +0530

    this is commit

---
+++ app/src/main/java/com/myapp/myapplication/MainActivity.java
@@ -0,0 +1,101 @@
+package com.myapp.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.myapp.myapplication.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+        private ImageView iconImage;
+        private LinearLayout linearLayout;
+        private Button register;
+        private Button login;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            iconImage = findViewById(R.id.icon_image);
+            linearLayout = findViewById(R.id.linear_layout);
+            register = findViewById(R.id.register);
+            login = findViewById(R.id.login);
+
+            linearLayout.animate().alpha(0f).setDuration(10);
+
+            TranslateAnimation animation = new TranslateAnimation(0 , 0 , 0 , -1500);
+            animation.setDuration(1000);
+            animation.setFillAfter(false);
+            animation.setAnimationListener(new MyAnimationListener());
+
+            iconImage.setAnimation(animation);
+
+            register.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this , RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                }
+            });
+
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(MainActivity.this , LoginActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                }
+            });
+
+        }
+
+        private class MyAnimationListener implements Animation.AnimationListener {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                iconImage.clearAnimation();
+                iconImage.setVisibility(View.INVISIBLE);
+                linearLayout.animate().alpha(1f).setDuration(1000);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+
+            if (FirebaseAuth.getInstance().getCurrentUser() != null){
+                startActivity(new Intent(MainActivity.this , HomeActivity.class));
+                finish();
+            }
+        }
+}
